f62dc404d2464bbeec3857b8ee3ff511d788e878
kiethongngu <21522263@gm.uit.edu.vn>
2023-09-26 22:13:33 +0700

    Update library, service, style color

--- StudentManagementPrj/Model/Const.cs
+++ StudentManagementPrj/Model/Const.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StudentManagementPrj.ViewModel;
 
 namespace StudentManagementPrj.Model
 {
-    internal class Const
+    public class Const : BaseViewModel
     {
+        public static int ID { get; set; }
+        public static string USERNAME { get; set; }
+        public static int KeyID { get; set; }
+        public static bool IsAdmin { get; set; } = false;
+        public static string AVA { get; set; }
+        public static string SchoolYear { get { return "2022-2023"; } }
+        public static string School { get { return "THPT ĐỐC BINH KIỀU"; } }
+        public static int Semester { get { return 1; } }
+        public static string _localLink = System.Reflection.Assembly.GetExecutingAssembly().Location.Remove(System.Reflection.Assembly.GetExecutingAssembly().Location.IndexOf(@"bin\Debug"));
     }
 }
+
